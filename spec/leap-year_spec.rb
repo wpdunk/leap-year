cfc496a49657b49191f8d30cf184f1f2fd420ef8
William Dunk <williamdunk@gmail.com>
2019-01-03 10:41:16 +0000

    added first test

---
+++ spec/leap-year_spec.rb
@@ -0,0 +1,9 @@
+describe 'leap-year' do
+  # All years divisible by 400 ARE leap years (e.g. 2000 was a leap year)
+  it 'greets Rico' do
+    expect(leap_year(400)).to eq true
+  end
+  # All years divisible by 100 but not by 400 are NOT leap years (e.g. 1700, 1800 and 1900 were not leap years)
+  # All years divisible by 4 and not by 100 ARE leap years (e.g. 2004, 2008 and 2012 were leap years)
+  # All years not divisible by 4 are NOT leap years (e.g. 2009, 2010 and 2011 were not leap years)
+end
